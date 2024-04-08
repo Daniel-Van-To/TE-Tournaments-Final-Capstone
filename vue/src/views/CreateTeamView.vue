ca7007c85548e149fb761e4574091ef3a6c5dda8
Daniel To <dannyvto96@gmail.com>
2024-04-08 12:06:19 -0500

    Corrected the CreateTeamView to display correctly

--- vue/src/views/CreateTeamView.vue
+++ vue/src/views/CreateTeamView.vue
@@ -27,15 +27,14 @@
   
   export default {
     components: {
-      NavBar,
-      CreateTeamService
+      NavBar
     },
     data() {
       return {
         team: {
             team_name: "",
             game_name: "",
-            current_user: this.$store.user.username,
+            current_user: this.$store.state.user.username,
             is_accepting_members: true
         }
       }
@@ -94,3 +93,46 @@
     }
   };
   </script>
+
+  
+<style scoped>
+.createTeamForm {
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.teamFormField {
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
+
+label {
+  display: inline-block;
+  margin-bottom: 0.5rem;
+}
+
+.form-control {
+  display: block;
+  width: 80%;
+  height: 30px;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+}
+
+textarea.form-control {
+  height: 75px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+select.form-control {
+  width: 20%;
+  display: inline-block;
+  margin: 10px 20px 10px 10px;
+}
+
+</style>
