7692c2d7ff40ee7775fbd65c9e367405ab8d2c14
Daniel To <dannyvto96@gmail.com>
2024-04-16 20:04:42 -0500

    Added TeamCard to mimick TournamentCard for Browse Teams

--- vue/src/views/TempBrowseTournamentsView.vue
+++ vue/src/views/TempBrowseTournamentsView.vue
@@ -1,103 +1,49 @@
 <template>
     <div class="home">
-      <h1>Browse Tournaments</h1>
-      <button @click.prevent="pushToCreateTournamentView">Create A Tournament</button>
-      <br><br/>
-      <label for="game-filter">Filter Tournaments by Game: </label>
-      <select class="game-filter" v-model="this.gameFilter">  
-        <option v-for="(game,index) in games" v-bind:value="game.name" v-bind:key="index">
-            {{ game.name }}
-        </option>
-      </select>
-        <TournamentCard class="card" v-for="tournament in filterTournamentsByGame" v-bind:key="tournament.tournamentId"
-            v-bind:tournament="tournament"/>
-        
+      <h1>Browse Teams</h1>
+      <TeamCard v-for="(team,index) in teams" v-bind:key="index" v-bind:team="team"/>
+      <p></p>
     </div>
   </template>
-    
+  
   <script>
-  import TournamentCard from '../components/browseTournamentsOrganizer/TournamentCard.vue';
-  import TournamentService from '../services/TournamentService';
-  import GameService from '../services/GameService.js';
+
+  import TeamsList from '../components/TeamsList.vue';
+  import TeamService from '../services/TeamService.js';
+  import TeamCard from '../components/TeamCard.vue';
   
   export default {
-  
-  
-    components: { TournamentCard },
-  
+    components: {
+      TeamCard
+    },
     data() {
       return {
-        tournaments: [],
-        games: [],
-        // tournamentStatus: ['Reset Filter', 'Completed', 'Ongoing', 'scheduled'],
-        gameFilter: ''
-        
+        teams: [],
       }
-    },
-  
-    computed: {
-  
-      filterTournamentsByGame() {
-            if(this.gameFilter.length > 0 && !(this.gameFilter === "Reset Filter")) {
-                return this.tournaments.filter((tournament) => {   
-                return tournament.gameName == this.gameFilter;
-                });    
-            }
-            else {
-                return this.tournaments;
-            }
-        },
     },
   
     methods: {
-  
-      pushToCreateTournamentView() {
-        this.$router.push({ name: 'create-tournament-view' });
+      
+      getTeams() {
+        TeamService.getTeams()
+        .then((response) => {
+          this.teams = response.data;
+        })
+        .catch((error) => {
+          if (error.response) {
+            this.$store.commit('SET_NOTIFICATION',
+              "Error getting teams list. Response received was '" + error.response.statusText + "'.");
+          } else if (error.request) {
+            this.$store.commit('SET_NOTIFICATION', "Error getting teams list. Server could not be reached.");
+          } else {
+            this.$store.commit('SET_NOTIFICATION', "Error getting teams list. Request could not be created.");
+          }
+        });
       }
   
     },
-    
-  
     created() {
-      TournamentService.getAllTournaments()
-      .then(response => {
-        this.tournaments = response.data;
-      });
-
-      GameService.getGamesList()
-      .then((response) => {
-        this.games = response.data;
-      });
-  
-  
+      this.getTeams();
     }
-  
-  }
-
-
+  };
   </script>
-  
-  <style scoped>
-  
-  .tournamentSections {
-    display: flex;
-    flex-direction:column;
-    border: 1px solid black;
-    flex-basis: 100%;
-    padding: 0;
-  
-  
-  }
-  
-  .tournamentSections>* {
-    border: 2px dashed blue;
-    padding-bottom: 20px;
-  }
-  
-  .card {
-    margin-bottom: 1rem;
-  }
-  
-  
-  
-  </style>
