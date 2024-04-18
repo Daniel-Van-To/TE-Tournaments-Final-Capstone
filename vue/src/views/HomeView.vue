497f77a8ed4e05c4a5fed03853d1d2dbcc8cb3ab
Daniel To <dannyvto96@gmail.com>
2024-04-17 19:27:26 -0500

    Added a carousel library that works; Got images to show nicely on home view

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,27 +1,72 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <slide-show/>
+    <!-- <slide-show/> -->
+    <vueper-slides>
+      <vueper-slide 
+      v-for="(slide,i) in slides"
+      v-on:click="pushToBrowseTournaments" 
+      :key="i" 
+      :title="slide.title"
+      :image="slide.image"/>
+    </vueper-slides>
   </div>
 </template>
 
 <script>
 import SlideShow from '../components/SlideShow.vue';
+import {VueperSlides, VueperSlide} from 'vueperslides';
+import 'vueperslides/dist/vueperslides.css';
 
 export default {
 
   components: {
-    SlideShow
+    // SlideShow,
+    VueperSlides,
+     VueperSlide
   },
 
   data() {
     return {
+
+      slideIndex: 0,
+      slides: [
+        {
+          title: 'Slide #1',
+          content: 'Slide 1 content',
+          image: '/florian-cordier-zOTpWmLyxdU-unsplash.jpg'
+        },
+        {
+          title: 'Slide #2',
+          content: 'Slide 2 content',
+          image: '/xavier-foucrier-UlGCxZVw078-unsplash.jpg'
+        },
+        {
+          title: 'Slide #3',
+          content: 'Slide 3 content',
+          image: '/carouselFootball.jpg'
+        },
+        {
+          title: 'Slide #4',
+          content: 'Slide 4 content',
+          image: '/pexels-patrick-case-3628912.jpg'
+        },
+        {
+          title: 'Slide #5',
+          content: 'Slide 5 content',
+          image: '/josh-hemsley-e-f17L4EBBc-unsplash.jpg'
+        },
+        
+      ]
 
     }
   },
 
 
   methods: {
+    pushToBrowseTournaments() {
+      this.$router.push({name: 'browse-tournaments'});
+    }
 
   },
   created() {
