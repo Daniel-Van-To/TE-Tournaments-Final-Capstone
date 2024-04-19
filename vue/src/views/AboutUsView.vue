e8a5225531c64bda215e617ee7e62f24d6a509be
Harry Simpson <harry.simpson324@gmail.com>
2024-04-19 01:39:58 -0400

    lol

--- vue/src/views/AboutUsView.vue
+++ vue/src/views/AboutUsView.vue
@@ -3,30 +3,38 @@
     <h1>About Us</h1>
     <p></p>
     <div class="theHomies">
-    <section class="oneOfUs">
-      <img>
-      <div class="reverser">
-        <h1>Revathi Ajjarapu</h1>
-        <p> description </p>
-      </div>
-    </section>
-    <section class="oneOfUs">
-      <img>
-      <div class="reverser">
-        <h1>Daniel To</h1>
-        <p> description </p>
-      </div>
-    </section>
-    <section class="oneOfUs">
-      <img src="../assets/images/Selected/xXxDablorDxXx.jpg">
-      <div class="reverser">
-        <h1>Harry Simpson</h1>
-        <p> Just a fella with some ambiguous food-like artwork near his workstation.
-          I got a lot of questions about that painting throughout the cohort. Its saturn. </p>
-          <p> I just like dabbin' </p>
-      </div>
-    </section>
-  </div>
+      <section class="oneOfUs">
+        <img>
+        <div class="reverser">
+          <h1>Revathi Ajjarapu</h1>
+          <p> description </p>
+        </div>
+      </section>
+      <section class="oneOfUs">
+        <img src="../assets/images/Selected/Linked in profile picture.jpg">
+        <div class="reverser">
+          <h1>Daniel To</h1>
+          <p> Hi my name is Daniel. I attended the University of Texas at Arlington
+            where I received both my Bachelors and Masters in Biomedical Engineering.
+            After college, I joined a startup company as a Laser Field Service Technician
+            where I was responsible for scheduling service with the hospital staff, and
+            completing laser maintenance and repairs.</p>
+          <p> I studied Java for the 4 years I was in High school, took several courses in C in college,
+            and even taught my self Python to complete my Senior capstone project at the university. Looking forward
+          to what comes next! </p>
+        </div>
+      </section>
+      <section class="oneOfUs">
+        <img src="../assets/images/Selected/xXxDablorDxXx.jpg">
+        <div class="reverser">
+          <h1>Harry Simpson</h1>
+          <p> Just a fella with some ambiguously food-like artwork near his workstation.
+            I got a lot of questions about that painting throughout the cohort. It's a painting of saturn. </p>
+          <p> I'm big into tournaments and dabbin'. Check out my cool tournament hosting service.
+            It's bloody tourna-ment-oh! </p>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
   
@@ -52,7 +60,6 @@ export default {
 </script>
 
 <style scoped>
-
 .oneOfUs {
   display: flex;
   border: 2px solid var(--purple);
@@ -74,6 +81,4 @@ export default {
   margin: 1rem;
   justify-self: left;
 }
-
-
 </style>
